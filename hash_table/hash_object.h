163638f88128f9bfba05e26ecd46cd2b2c696e09
zachws <zacharywoodssmith@gmail.com>
2022-01-22 17:41:01 -0700

    Added header file contents for hash_table.h and for hash_object.h

---
+++ hash_table/hash_object.h
@@ -0,0 +1,43 @@
+#ifndef PROJECT_HASH_OBJECT_H
+#define PROJECT_HASH_OBJECT_H
+
+#include <string> //for std::string
+
+namespace zw {
+
+class HashObject {
+ public:
+ //Default constructor
+  HashObject() {}
+  //Default destructor
+  ~HashObject() {}
+  //no copy constructor i.e. delete
+  HashObject(const HashObject &) = delete;
+  //Default assignment/operator constructor
+  HashObject &operator=(const HashObject &) = default;
+  //Returns key_ for caller
+  std::string &GetKey() { return key_; }
+  //Returns value_ for caller
+  std::string &GetValue() { return value_; }
+  //Set Dummy Key and Value (Node)
+  void SetAsDummy() {
+    key_ = GetDummyKey();
+    value_ = "";
+  }
+  //Setter for the key_ 
+  void SetKey(const std::string key) { key_ = key; }
+  //Setter for the _value
+  void SetValue(const std::string value) { value_ = value; }
+  //Shows whether key is Dummy or not I think 
+  static const std::string GetDummyKey() { return "<Dummy>"; }
+  //Returns the string that the key is infact null 
+  static const std::string GetNullKey() { return "<Null>"; }
+
+ private:
+  std::string key_;
+  std::string value_;
+};
+
+}  // namespace jw
+
+#endif  // PROJECT_HASH_OBJECT_H
--- hash_table/hash_table.h
+++ hash_table/hash_table.h
@@ -1,1 +1,44 @@
+#include <assert.h>
+#include <stdlib.h>  // abs
+#include <iostream>
+#include <string>
+#include "hash_object.h"
 
+#ifndef PROJECT_HASH_TABLE_H
+#define PROJECT_HASH_TABLE_H
+
+namespace zw {
+class HashTable {
+ public:
+  // our user-defined constructor
+  explicit HashTable(const int size);
+  //Destructor (need to implement in the hash_table.cpp file)
+  ~HashTable();
+  //No copy constructor i.e. delete
+  HashTable(const HashTable &) = delete;
+  //No assignment operator constructor i.e. delete
+  HashTable &operator=(const HashTable &) = delete;
+
+  // Add the given key and object to hash table. If key exists, update the value.
+  void Add(HashObject *object);
+  // Returns true if the given key exists in the table
+  bool Exists(const std::string key);
+  // Returns the value associated with the given key, or NULL if it doesn't
+  // exist
+  const std::string Get(const std::string key);
+  // Removes the value associated with key from the table.
+  void Remove(const std::string key);
+  // Outputs the content of the hash table for debugging purposes.
+  void PrintDebug();
+
+ private:
+  //size of the hash table
+  int size_;
+  //Pointer to data, held in hash_object.h 
+  HashObject *data_;
+
+  int Hash(const std::string key);
+};
+}//end of namespace zw
+
+#endif  // PROJECT_HASH_TABLE_H
